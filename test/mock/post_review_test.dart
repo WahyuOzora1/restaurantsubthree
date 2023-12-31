69c99a8b56c38174db3f516d04e774eb4d4b2849
wahyu restu pamuji <wahyurestup1@gmail.com>
2023-12-31 12:27:40 +0700

    fixing testing

--- test/mock/post_review_test.dart
+++ test/mock/post_review_test.dart
@@ -14,14 +14,14 @@ void main() {
         'returns true if the http call completes successfully (status code 201)',
         () async {
       final client = MockClient();
+      final customerReview = CustomerReviewRequest(
+          id: 'w9pga3s2tubkfw1e867', name: 'Dono', review: 'Keren Sekali');
 
-      when(client.post(Uri.parse('https://restaurant-api.dicoding.dev/review')))
+      when(client.post(Uri.parse('https://restaurant-api.dicoding.dev/review'),
+              body: customerReview.toJson()))
           .thenAnswer((_) async => http.Response(
               '{"error":false,"message":"success","customerReviews":[{"name":"Widdy","review":"Tidak ada duanya!","date":"13 Juli 2019"},{"name":"Ahmad","review":"Tidak rekomendasi untuk pelajar!","date":"13 November 2019"},{"name":"Gilang","review":"Saya sangat suka menu malamnya!","date":"13 Juli 2019"},{"name":"Anonymous","review":"test","date":"29 Desember 2023"},{"name":"Anonymous","review":"test","date":"29 Desember 2023"},{"name":"Dono","review":"Keren Sekali","date":"29 Desember 2023"},{"name":"Dono","review":"Keren Sekali","date":"29 Desember 2023"}]}',
               201));
-
-      final customerReview = CustomerReviewRequest(
-          id: 'w9pga3s2tubkfw1e867', name: 'Dono', review: 'Keren Sekali');
 
       final result =
           await ApiService().postReviewRestaurant(customerReview, client);
